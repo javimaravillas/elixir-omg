0833b46e957722d8220c02da3c14b74acdf7ee54
ino.murko@outlook.com <ino.murko@outlook.com>
2020-09-02 10:22:38 +0200

    rm mix based chch

--- apps/omg_watcher/test/omg_watcher/integration/invalid_exit2_test.exs
+++ apps/omg_watcher/test/omg_watcher/integration/invalid_exit2_test.exs
@@ -94,44 +94,4 @@ defmodule OMG.Watcher.Integration.InvalidExitTest do
     IntegrationTest.wait_for_byzantine_events([], @timeout)
   end
 
-  @tag fixtures: [:in_beam_watcher, :stable_alice, :mix_based_child_chain, :token, :stable_alice_deposits]
-  test "invalid_exit without a challenge raises unchallenged_exit after sla_margin had passed, can be challenged",
-       %{stable_alice: alice, stable_alice_deposits: {deposit_blknum, _}} do
-    %{"blknum" => first_tx_blknum} =
-      OMG.TestHelper.create_encoded([{deposit_blknum, 0, 0, alice}], @eth, [{alice, 9}]) |> WatcherHelper.submit()
-
-    %{"blknum" => second_tx_blknum} =
-      OMG.TestHelper.create_encoded([{first_tx_blknum, 0, 0, alice}], @eth, [{alice, 8}]) |> WatcherHelper.submit()
-
-    IntegrationTest.wait_for_block_fetch(second_tx_blknum, @timeout)
-
-    %{"txbytes" => txbytes, "proof" => proof, "utxo_pos" => tx_utxo_pos} =
-      WatcherHelper.get_exit_data(first_tx_blknum, 0, 0)
-
-    {:ok, %{"status" => "0x1", "blockNumber" => eth_height}} =
-      RootChainHelper.start_exit(tx_utxo_pos, txbytes, proof, alice.addr)
-      |> DevHelper.transact_sync!()
-
-    IntegrationTest.wait_for_byzantine_events([%Event.InvalidExit{}.name], @timeout)
-
-    exit_processor_sla_margin = Application.fetch_env!(:omg_watcher, :exit_processor_sla_margin)
-    DevHelper.wait_for_root_chain_block(eth_height + exit_processor_sla_margin, @timeout)
-    IntegrationTest.wait_for_byzantine_events([%Event.InvalidExit{}.name, %Event.UnchallengedExit{}.name], @timeout)
-
-    # after the notification has been received, a challenged is composed and sent, regardless of it being late
-    challenge = WatcherHelper.get_exit_challenge(first_tx_blknum, 0, 0)
-
-    assert {:ok, %{"status" => "0x1"}} =
-             RootChainHelper.challenge_exit(
-               challenge["exit_id"],
-               challenge["exiting_tx"],
-               challenge["txbytes"],
-               challenge["input_index"],
-               challenge["sig"],
-               alice.addr
-             )
-             |> DevHelper.transact_sync!()
-
-    IntegrationTest.wait_for_byzantine_events([], @timeout)
-  end
 end
