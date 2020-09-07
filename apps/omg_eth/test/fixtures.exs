872c643575041072aeb1b71a77820536080c46ed
Ino Murko <ino.murko@outlook.com>
2020-09-07 23:33:21 +0200

    override env var

--- apps/omg_eth/test/fixtures.exs
+++ apps/omg_eth/test/fixtures.exs
@@ -29,24 +29,22 @@ defmodule OMG.Eth.Fixtures do
   @test_erc20_vault_id 2
 
   deffixture eth_node do
-    if Application.get_env(:omg_eth, :run_test_eth_dev_node, true) do
-      {:ok, exit_fn} = DevNode.start()
-
-      on_exit(exit_fn)
-    end
-
-    :ok
-  end
-
-  deffixture contract(eth_node) do
     case System.get_env("DOCKER_GETH") do
       "true" ->
         :ok
 
       _ ->
-        :ok = eth_node
-    end
+        if Application.get_env(:omg_eth, :run_test_eth_dev_node, true) do
+          {:ok, exit_fn} = DevNode.start()
 
+          on_exit(exit_fn)
+        end
+
+        :ok
+    end
+  end
+
+  deffixture contract(eth_node) do
     {:ok, true} =
       Ethereumex.HttpClient.request("personal_unlockAccount", ["0x6de4b3b9c28e9c3e84c2b2d3a875c947a84de68d", "", 0], [])
 
