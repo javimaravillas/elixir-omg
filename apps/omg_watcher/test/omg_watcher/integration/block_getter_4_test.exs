a5dd3b30c26dc282478cb35b76de4c4d83e58aa8
Ino Murko <ino.murko@outlook.com>
2020-09-08 17:22:52 +0200

    extract more tests

--- apps/omg_watcher/test/omg_watcher/integration/block_getter_4_test.exs
+++ apps/omg_watcher/test/omg_watcher/integration/block_getter_4_test.exs
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-defmodule OMG.Watcher.Integration.BlockGetterTest do
+defmodule OMG.Watcher.Integration.BlockGetter4Test do
   @moduledoc """
   This test is intended to be the major smoke/integration test of the Watcher
 
--- apps/omg_watcher/test/omg_watcher/integration/invalid_exit_1_test.exs
+++ apps/omg_watcher/test/omg_watcher/integration/invalid_exit_1_test.exs
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-defmodule OMG.Watcher.Integration.InvalidExitTest do
+defmodule OMG.Watcher.Integration.InvalidExit1Test do
   use ExUnitFixtures
   use ExUnit.Case, async: false
   use OMG.Fixtures
