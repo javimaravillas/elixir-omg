238ad408751614a1e9668a9788afb1e0854bd4cf
Ino Murko <ino.murko@outlook.com>
2020-09-08 14:06:31 +0200

    what are we waiting for?

--- apps/omg_watcher/test/support/integration/test_helper.ex
+++ apps/omg_watcher/test/support/integration/test_helper.ex
@@ -40,11 +40,10 @@ defmodule OMG.Watcher.Integration.TestHelper do
   def wait_for_block_fetch(block_number, timeout) do
     # TODO query to State used in tests instead of an event system, remove when event system is here
     fn ->
-      IO.inspect(State.get_status(), label: "wait_for_block_fetch")
-
-      if elem(State.get_status(), 0) <= block_number,
-        do: :repeat,
-        else: {:ok, block_number}
+      case elem(State.get_status(), 0) do
+        blknum when blknum < block_number -> :repeat
+        _ -> {:ok, block_number}
+      end
     end
     |> WaitFor.ok(timeout)
 
