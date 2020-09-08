898022dc45b0431a72f36525652d0c68bb6a112f
Ino Murko <ino.murko@outlook.com>
2020-09-08 17:10:52 +0200

    extract more tests

--- apps/omg_watcher/test/omg_watcher/integration/invalid_exit_2_test.exs
+++ apps/omg_watcher/test/omg_watcher/integration/invalid_exit_2_test.exs
@@ -50,9 +50,8 @@ defmodule OMG.Watcher.Integration.InvalidExit2Test do
     %{"blknum" => second_tx_blknum} =
       [{first_tx_blknum, 0, 0, alice}] |> OMG.TestHelper.create_encoded(@eth, [{alice, 8}]) |> WatcherHelper.submit()
 
-    IO.inspect(second_tx_blknum, label: "second_tx_blknum")
     IntegrationTest.wait_for_block_fetch(second_tx_blknum, @timeout)
-    Process.sleep(5000)
+    Process.sleep(10_000)
 
     exit_data = WatcherHelper.get_exit_data(first_tx_blknum, 0, 0)
     %{"txbytes" => txbytes, "proof" => proof, "utxo_pos" => tx_utxo_pos} = exit_data
